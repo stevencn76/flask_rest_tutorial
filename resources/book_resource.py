a2260d6d4c2ff9caeb05df28ccdb3f19ff16d88e
Steven Chen <stevencn76@gmail.com>
2023-12-23 03:53:40 -0800

    create and update records

--- resources/book_resource.py
+++ resources/book_resource.py
@@ -1,4 +1,9 @@
+from datetime import datetime
+
+from flask import request
 from flask_restful import Resource
+
+from models.book_model import BookModel
 from resources import api
 from services.book_service import BookService
 
@@ -11,8 +16,47 @@ class BookResource(Resource):
         else:
             return {'error': f'Book not found for id: {book_id}'}, 404
 
-    def put(self, student_id: int):
-        return {'id': student_id, 'name': 'Mary', 'gender': 'female'}
+    def put(self, book_id: int):
+        try:
+            request_json = request.json
+            if request_json:
+                name = request_json.get('name', None)
+                author = request_json.get('author', None)
+                publish_time_str = request_json.get('publish_time', None)
+                publish_time = datetime.fromisoformat(publish_time_str) if publish_time_str else None
+
+                book_model = BookModel(id=book_id, name=name, author=author, publish_time=publish_time)
+                book_model = BookService().update_book(book_model)
+
+                return book_model.serialize()
+            else:
+                return {'error': 'Please provide book info as a json'}, 400
+        except Exception as error:
+            return {'error': f'{error}'}, 400
+
+
+class BookListResource(Resource):
+    def get(self):
+        book_list = BookService().get_all_books()
+        return [book_model.serialize() for book_model in book_list]
+
+    def post(self):
+        try:
+            request_json = request.json
+            if request_json:
+                name = request_json.get('name', None)
+                author = request_json.get('author', None)
+                publish_time = datetime.fromisoformat(request_json.get('publish_time', None))
+
+                book_model = BookModel(name=name, author=author, publish_time=publish_time)
+                BookService().create_book(book_model)
+
+                return book_model.serialize()
+            else:
+                return {'error': 'Please provide book info as a json'}, 400
+        except Exception as error:
+            return {'error': f'{error}'}, 400
 
 
 api.add_resource(BookResource, '/books/<int:book_id>')
+api.add_resource(BookListResource, '/books')
--- services/book_service.py
+++ services/book_service.py
@@ -1,3 +1,5 @@
+from sqlalchemy import Select, asc
+
 from models.book_model import BookModel
 from resources import db
 
@@ -5,3 +7,37 @@ from resources import db
 class BookService:
     def get_book_by_id(self, book_id: int):
         return db.session.get(BookModel, book_id)
+
+    def get_all_books(self):
+        query = Select(BookModel).order_by(asc(BookModel.name))
+        return db.session.scalars(query).all()
+
+    def get_book_by_name(self, book_name: str):
+        query = Select(BookModel).where(BookModel.name == book_name)
+        return db.session.scalars(query).all()
+
+    def create_book(self, book_model: BookModel):
+        exist_books = self.get_book_by_name(book_model.name)
+        if exist_books:
+            raise Exception(f'Book exists with name "{book_model.name}"')
+
+        db.session.add(book_model)
+        db.session.commit()
+
+        return book_model
+
+    def update_book(self, book_model: BookModel):
+        exist_book = self.get_book_by_id(book_model.id)
+        if not exist_book:
+            raise Exception(f'Book not found with id: {book_model.id}')
+
+        if book_model.name:
+            exist_book.name = book_model.name
+        if book_model.author:
+            exist_book.author = book_model.author
+        if book_model.publish_time:
+            exist_book.publish_time = book_model.publish_time
+
+        db.session.commit()
+
+        return exist_book
